74d537d79f1a9b16c25d891f9a31fcae9fe91ae0
Ansned <95335385+Ansned@users.noreply.github.com>
2022-10-28 10:12:06 +0800

    Update alidrive.js

--- Script/alidrive.js
+++ Script/alidrive.js
@@ -17,16 +17,4 @@
 [rewrite_local]
 
 # ～ 阿里云盘（2022-10-16）@ddgksf2013
-^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Script/alidrive.js
-
-[mitm]
-
-hostname=api.aliyundrive.com
-
-***********************************/
-
-let ddgksf2013 = JSON.parse($response.body);
-if (ddgksf2013.result) {
-    ddgksf2013.result = Object.values(ddgksf2013.result).filter(item => (item["appCode"]=="file" || item["appCode"]=="video"));
-}
-$done({ body: JSON.stringify(ddgksf2013) });
+^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://raw.githubusercontent.com/Ansned/Cuttlefish-QX/master/Script/alidrive.js
